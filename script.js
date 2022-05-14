9cf49cb745dfd7eb35b88f5ccefaff1e79dd18a7
Blastose <blastoisejay@gmail.com>
2022-05-13 21:27:25 -0700

    Implement adding books from modal input

--- script.js
+++ script.js
@@ -22,44 +22,121 @@ Library.prototype.printBooks = function() {
   });
 }
 
-function createBookCard(book) {
-  const div = document.createElement("div");
-  div.textContent = book.info();
-  div.classList.add("book-card");
-
-  return div;
+Library.prototype.getBook = function(index) {
+  return this.books[index];
 }
 
-const library = new Library();
+function DOM() {
+  this.modal = document.querySelector('.modal');
+  this.bookList = document.querySelector('.book-list');
+}
 
-const bookList = document.querySelector(".book-list");
+DOM.prototype.showModal = function() {
+  this.modal.classList.remove('modal-hide');
+  this.modal.classList.add('modal-show');
+}
+
+DOM.prototype.hideModal = function() {
+  this.modal.classList.remove('modal-show');
+  this.modal.classList.add('modal-hide');
+}
+
+DOM.prototype.clearBookList = function() {
+  while (this.bookList.firstChild) {
+    this.bookList.removeChild(this.bookList.lastChild);
+  }
+}
+
+DOM.prototype.displayBookList = function(library) {
+  this.clearBookList();
+  library.books.forEach((book, index) => {
+    const bookCard = createBookCard(book, index);
+    this.bookList.appendChild(bookCard);
+  });
+}
+
+function createElementWithClass(elementType, elementClass) {
+  const element = document.createElement(elementType);
+  element.classList.add(elementClass);
+  return element;
+}
+
+function createBookCard(book, id) {
+  const divBookCard = createElementWithClass("div", "book-card");
+  const divHighlight = createElementWithClass("div", "book-card-highlight");
+  const divBookInfo = createElementWithClass("div", "book-info");
+  const divBookTitle = createElementWithClass("div", "book-title");
+  const divBookAuthor = createElementWithClass("div", "book-author");
+  const divBookPages = createElementWithClass("div", "book-pages");
+
+  const divBookOptions = createElementWithClass("div", "book-options");
+  const divBookReadStatus = createElementWithClass("div", "book-read-status");
+  const labelToggleRead = document.createElement("label");
+  const buttonDeleteBook = createElementWithClass("button", "delete-book");
+  buttonDeleteBook.textContent = "Delete";
+  buttonDeleteBook.setAttribute("data-book-id", `${id}`);
+  labelToggleRead.setAttribute("for", `toggle-read-${id}`);
+  labelToggleRead.textContent = "Read?";
+
+  const inputCheckboxRead = document.createElement("input");
+  inputCheckboxRead.setAttribute("type", "checkbox");
+  inputCheckboxRead.setAttribute("id", `toggle-read-${id}`);
+  inputCheckboxRead.checked = book.read;
+
+  divBookCard.setAttribute("id", `book-${id}`);
+  divBookCard.appendChild(divHighlight);
+  divBookCard.appendChild(divBookInfo);
+  divBookCard.appendChild(divBookOptions);
+
+  divBookInfo.appendChild(divBookTitle);
+  divBookInfo.appendChild(divBookAuthor);
+  divBookInfo.appendChild(divBookPages);
+
+  divBookOptions.appendChild(divBookReadStatus);
+  divBookOptions.appendChild(buttonDeleteBook);
+  divBookReadStatus.appendChild(labelToggleRead);
+  divBookReadStatus.appendChild(inputCheckboxRead);
+
+  divBookTitle.textContent = `${book.title}`;
+  divBookAuthor.textContent = `by ${book.author}`;
+  divBookPages.textContent = `${book.numberOfPages} pages`;
+
+  return divBookCard;
+}
+
+const dom = new DOM();
+const library = new Library();
 
 const addBookButton = document.querySelector('.add-book');
 addBookButton.addEventListener('click', () => {
-  const book = new Book("The Hobbit", "J.R.R. Tolkien", Math.floor(Math.random() * 200) + 200);
-  library.addBookToLibrary(book);
-
-  bookList.appendChild(createBookCard(book));
-
-  const modal = document.querySelector('.modal');
-  modal.classList.remove('modal-hide');
-  modal.classList.add('modal-show');
+  dom.showModal();
 });
 
-
 const modalContainer = document.querySelector('.modal-container');
-const modal = document.querySelector('.modal');
+
 modalContainer.addEventListener('click', (e) => {
-  console.log(e.target);
-  console.log(e.currentTarget);
   if (e.target === e.currentTarget) {
-    modal.classList.remove('modal-show');
-    modal.classList.add('modal-hide');
+    dom.hideModal();
   }
 });
 
 const modalCancelButton = document.querySelector('.cancel-button');
 modalCancelButton.addEventListener('click', () => {
-  modal.classList.remove('modal-show');
-  modal.classList.add('modal-hide');
+  dom.hideModal();
+});
+
+const modalAddButtion = document.querySelector('.add-button');
+modalAddButtion.addEventListener('click', () => {
+  const modalBookTitle = document.querySelector('#modal-book-title');
+  const modalBookAuthor = document.querySelector('#modal-book-author');
+  const modalBookPages = document.querySelector('#modal-book-pages');
+
+  const book = new Book(modalBookTitle.value, modalBookAuthor.value, modalBookPages.value, false);
+  modalBookTitle.value = "";
+  modalBookAuthor.value = "";
+  modalBookPages.value = "";
+  
+  library.addBookToLibrary(book);
+  dom.displayBookList(library);
+  dom.hideModal();
 });
